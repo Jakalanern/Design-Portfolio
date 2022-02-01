2b3f444196190ac28e0052e6a4afac9bdb7a3f30
Jack Stevens <77250250+Jakalanern@users.noreply.github.com>
2022-01-31 18:23:31 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -13,79 +13,73 @@ let nav = document.querySelector("nav");
 intro.style.opacity = 1;
 
 burger.addEventListener("click", function () {
+  burgerClick();
+  dropdownAnim();
+});
+
+dropdownX.addEventListener("click", function () {
+  xClick();
+});
+
+for (let link of dropdownLink) {
+  link.addEventListener("click", function () {
+    xClick();
+  });
+}
+
+function burgerClick() {
   dropdownX.style.display = "initial";
   dropdown.style.top = "0%";
   nav.style.color = "white";
   for (let l of line) {
     l.style.background = "white";
   }
+}
 
+function dropdownAnim() {
   setTimeout(function () {
+    dropdownLink[0].style.transform = "translateX(0)";
+    dropdownLink[0].style.transition = "transform 0.5s ease, opacity 1.1s";
+
+    dropdownLink[0].style.opacity = "1";
     setTimeout(function () {
-      dropdownLink[0].style.transform = "translateX(0)";
-      dropdownLink[0].style.transition = "transform 0.5s ease, opacity 1.1s";
+      dropdownLink[1].style.transform = "translateX(0)";
+      dropdownLink[1].style.transition = "transform 0.5s ease, opacity 1.1s";
 
-      dropdownLink[0].style.opacity = "1";
-      setTimeout(function () {
-        dropdownLink[1].style.transform = "translateX(0)";
-        dropdownLink[1].style.transition = "transform 0.5s ease, opacity 1.1s";
-
-        dropdownLink[1].style.opacity = "1";
-
-        setTimeout(function () {
-          dropdownLink[2].style.transform = "translateX(0)";
-          dropdownLink[2].style.transition =
-            "transform 0.5s ease, opacity 1.1s";
-
-          dropdownLink[2].style.opacity = "1";
-
-          setTimeout(function () {
-            dropdownLink[3].style.transform = "translateX(0)";
-            dropdownLink[3].style.transition =
-              "transform 0s ease, opacity 1.25s";
-
-            dropdownLink[3].style.opacity = "1";
-          }, 300);
-        }, 100);
-      }, 125);
-    }, 150);
-
-    console.log("link transform");
-  }, 0);
-});
-
-dropdownX.addEventListener("click", function () {
-  setTimeout(function () {
-    dropdownLink[3].style.transition = "transform 0s ease, opacity .2s";
-    dropdownLink[3].style.opacity = "0";
-
-    setTimeout(function () {
-      dropdownLink[2].style.transform = "translateX(300%)";
-      dropdownLink[2].style.transition = "transform 0.5s ease, opacity .2s";
-
-      dropdownLink[2].style.opacity = "0";
+      dropdownLink[1].style.opacity = "1";
 
       setTimeout(function () {
-        dropdownLink[1].style.transform = "translateX(300%)";
-        dropdownLink[1].style.transition = "transform 0.5s ease, opacity .2s";
+        dropdownLink[2].style.transform = "translateX(0)";
+        dropdownLink[2].style.transition = "transform 0.5s ease, opacity 1.1s";
 
-        dropdownLink[1].style.opacity = "0";
+        dropdownLink[2].style.opacity = "1";
 
         setTimeout(function () {
-          dropdownLink[0].style.transform = "translateX(300%)";
-          dropdownLink[0].style.transition = "transform 0.5s ease, opacity .2s";
+          dropdownLink[3].style.transform = "translateX(0)";
+          dropdownLink[3].style.transition =
+            "border 0.25s, color 0.25s, background 0.25s, transform 0s, opacity 1.25s, scale 0.25s";
 
-          dropdownLink[0].style.opacity = "0";
-        }, 150);
-      }, 125);
-    }, 100);
-  }, 0);
+          dropdownLink[3].style.opacity = "1";
+        }, 300);
+      }, 100);
+    }, 125);
+  }, 150);
+}
+
+function xClick() {
+  for (let link of dropdownLink) {
+    link.style.transition = "opacity .5s";
+    link.style.opacity = 0;
+  }
 
   setTimeout(function () {
+    for (let link of dropdownLink) {
+      link.style.transform = "translateX(400%)";
+    }
     dropdown.style.top = "-100%";
     nav.style.color = "black";
     for (let l of line) {
       l.style.background = "black";
     }
-  }, 500);
-});
+  }, 0);
+}
